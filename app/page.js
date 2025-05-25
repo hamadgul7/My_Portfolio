805b93ebf56334180d96fcebb5af18197fe0f9ba
Hamad Gul <hamadgul025@gmail.com>
2025-05-26 02:08:19 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -29,8 +29,8 @@ export default async function Home() {
       <HeroSection />
       <AboutSection />
       <Experience />
-      {/* <Skills />
-      <Projects />
+      <Skills />
+      {/* <Projects />
       <Education />
       <ContactSection /> */}
     </div>
