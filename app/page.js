97414a01a6762b2ce06ad239fb372fd6b080bd6c
Hamad Gul <hamadgul025@gmail.com>
2025-05-26 01:41:04 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -27,12 +27,12 @@ export default async function Home() {
   return (
     <div suppressHydrationWarning >
       <HeroSection />
-      {/* <AboutSection />
+      <AboutSection />
       <Experience />
-      <Skills />
+      {/* <Skills />
       <Projects />
-      <Education />
-      <ContactSection /> */}
+      <Education /> */}
+      <ContactSection />
     </div>
   )
 };
